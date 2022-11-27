60a415956854073f496a5ac1ed0ad53023ed2440
Lawrence Ephrim <ephrimlawrence@gmail.com>
2022-11-27 18:16:25 +0000

    test(auth): update signup test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { faker } from '@faker-js/faker';
@@ -16,13 +16,20 @@ describe('AppController (e2e)', () => {
     nextOfKin: faker.name.fullName()
   }
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      stopAtFirstError: true,
+      skipMissingProperties: false,
+    }))
     await app.init();
+
   });
 
   it(`Create account => ${path} (POST)`, () => {
@@ -35,5 +42,28 @@ describe('AppController (e2e)', () => {
       .expect((resp) => resp.body.nextOfKin == form.nextOfKin)
       .expect((resp) => resp.body.password == undefined)
       .expect((resp) => resp.body.email == form.email);
+  });
+
+  it(`Create account with existing email => ${path} (POST)`, () => {
+    return request(app.getHttpServer())
+      .post(path)
+      .send(form)
+      .expect(HttpStatus.CONFLICT);
+  });
+
+  it(`Create account with invalid email => ${path} (POST)`, () => {
+    return request(app.getHttpServer())
+      .post(path)
+      .send({ ...form, email: faker.random.words() })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it(`with missing required fields  => ${path} (POST)`, () => {
+    const { email, firstName, nextOfKin, ...f } = form
+
+    return request(app.getHttpServer())
+      .post(path)
+      .send(f)
+      .expect(HttpStatus.BAD_REQUEST);
   });
 });
